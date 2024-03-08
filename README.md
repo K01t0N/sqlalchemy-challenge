e7e2b85d3d44a2eed13ea1fc64e643a5a6ddd6c0
K01t0N <Kolton101@gmail.com>
2024-03-08 14:53:25 -0600

    Updates to app.py and README.md

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # sqlalchemy-challenge
 
-This assignment performs queries using SQLAlchemy.
+This assignment performs queries using SQLAlchemy, then builds an app using flask.
 
 ## References
 
-#### All references are from climate_start.ipynb
-
+#### climate_start.ipynb
 
 **Code Blocks 10 and 16:**
 
@@ -30,3 +29,20 @@ Using the count function in SQLAlchemy with the group by function:
 
 https://stackoverflow.com/questions/1052148/group-by-count-function-in-sqlalchemy
 
+
+#### app.py
+
+**lines 60-63, 86-91**
+
+Finding the last row in SQLAlchemy:
+https://stackoverflow.com/questions/8551952/how-to-get-last-record
+
+Extracting the date from the last row:
+
+https://docs.python.org/3/library/datetime.html
+https://dev.to/lavary/about-attributeerror-module-datetime-has-no-attribute-strptime-in-python-1k53
+https://stackoverflow.com/questions/25015711/time-data-does-not-match-format
+
+Subtracting from a date:
+
+https://www.geeksforgeeks.org/how-to-add-and-subtract-days-using-datetime-in-python/
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,144 @@
 # Import the dependencies.
+import numpy as np
+import datetime as dt
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Stations = Base.classes.stations
 
 # Create our session (link) from Python to the DB
+session = Session(engine)
+
+def query_to_dict(query, dict):
+    i = 1
+    for row in query:
+        dict[i] = [row[0], row[1]]
+        i += 1
 
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitaion():
+
+    # finding one year before the last date of the dataset
+    last_row = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    last_date = dt.datetime.strptime(last_row[0], "%Y-%m-%d")
+    date_difference = last_date - dt.timedelta(days=365)
+
+    # retrieving the precipitation amounts
+    precipitation_query = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= date_difference).\
+        order_by(Measurement.date.desc()).all()
+
+    p_dict = {}
+    query_to_dict(precipitation_query, p_dict)
+    return jsonify(p_dict)
+
+@app.route("/api/v1.0/stations")
+def precipitaion():
+
+    # retrieiving the stations
+    station_query = session.query(Stations.station, Stations.name)
+    station_dict = {}
+    query_to_dict(station_query, station_dict)
+    return jsonify(station_dict)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+    # finding one year before the last date for the station
+    station_last_row = session.query(Measurement.date).\
+                               filter(Measurement.station == 'USC00519281').\
+                               order_by(Measurement.date.desc()).first()
+    station_last_date = dt.datetime.strptime(station_last_row[0], "%Y-%m-%d")
+    station_date_difference = station_last_date - dt.timedelta(days=365)
+
+    # retrieving the temperatures
+    tobs_query = session.query(Measurement.station, Measurement.tobs).\
+                        filter((Measurement.station == 'USC00519281') and \
+                              (Measurement.date >= station_date_difference)).\
+                      order_by(Measurement.date.desc())
+
+    tobs_dict = {}
+    query_to_dict(tobs_query, tobs_dict)
+    return jsonify(tobs_dict)
+
+@app.route("/api/v1.0/<start>")
+def start():
+
+    # retrieving the temperature aggregates
+    start_query = session.query(Measurement.date,
+                                Measurement.station, 
+                                sqlalchemy.func.min(Measurement.tobs), \
+                                sqlalchemy.func.max(Measurement.tobs),  \
+                                sqlalchemy.func.avg(Measurement.tobs),).\
+                          filter(Measurement.date >= start)
+
+    start_dict = {}
+    i = 1
+    for row in start_query:
+        dict[i] = [row[0], row[1], row[2], row[3], row[4]]
+        i += 1
+
+    return jsonify(start_dict)
+
+@app.route("/api/v1.0/<start>/<end>")
+def end():
+
+    # retrieving the temperature aggregates
+    end_query = session.query(Measurement.date,
+                              Measurement.station, 
+                              sqlalchemy.func.min(Measurement.tobs), \
+                              sqlalchemy.func.max(Measurement.tobs),  \
+                              sqlalchemy.func.avg(Measurement.tobs),).\
+                        filter((Measurement.date >= start) and \
+                              (Measurement.date <= end))
+
+    end_dict = {}
+
+    i = 1
+    for row in end_query:
+        dict[i] = [row[0], row[1], row[2], row[3], row[4]]
+        i += 1
+
+    return jsonify(end_dict)
+
+if __name__ == '__main__':
+    app.run(debug=True)
